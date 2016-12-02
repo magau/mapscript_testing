a9ddbb6b4242388cad3e712c8b46576ea5225447
macedoj <joao.macedo@ipma.pt>
2016-12-02 02:47:16 +0000

    Code cleanup...

--- mapscript-wsgi.py
+++ mapscript-wsgi.py
@@ -1,26 +1,59 @@
 #!/usr/bin/env python
 #-*-coding:utf8-*-
-
-# This is a python-mapscript adaptation to serve
+"""# This is a python-mapscript adaptation to serve
 # the example_1.1 map of the mapserver tutorial.
 # (http://mapserver.org/tutorial/example1-1.html#example1-1)
-# 
-# Reference websites:
+# Download the tutorial data from:
+# http://download.osgeo.org/mapserver/docs/mapserver-tutorial.zip
+ 
+# Other reference websites:
 #     http://mapserver.org/tutorial/index.html
 #     https://github.com/mapserver/mapserver/wiki
 #     https://gist.github.com/tomkralidis/9adbd4864c03647aa7eb4f96a3c33297
+
+# The application is suposed to return a png map
+# as response for the given url:
+#
+# localhost/ms-wsgi?service=WMS&version=1.3.0&request=GetMap
+#
+# For that default request parameters were here given.
+
+# Copy the example1-1.map mapfile, from the
+# totorial downloaded folder, to the base_path
+# location:
 # 
-# Download tutorial data:
-# http://download.osgeo.org/mapserver/docs/mapserver-tutorial.zip
+# Original location:
+#     ms4w/apps/tutorial/htdocs/example1-1.map
+# 
+# The atribute SHAPEPATH of the mapfile copy
+# must be change according with the new location 
+# of the tutorial folder 'data' directory.
+# (relative path is here used)
+# 
+# SHAPEPATH      "ms4w/apps/tutorial/data"
+ 
+# The folowing tags had been added to the
+# MAP tag of the original mapfile:
+# 
+# PROJECTION # Projection definition:
+#    "init=epsg:4326"
+# END # End of the output Projection definition ---
+# 
+# WEB
+#   METADATA
+#     "ows_enable_request" "*"
+#   END
+# END
+"""
 
 import os
 import sys
 
 import mapscript
 
-# Edit the local_settings according with yours:
-local_settings = {
-    'base_path':'/home/magau/dev_repos/mapscript_testing'
+# Edit the 'base_path' value according with yours:
+settings_local = {
+    'base_path':'/<user>/<base>/<dir>'
 }
 
 
@@ -37,46 +70,14 @@ MAPSERV_ENV = [
 
 
 def application(env, start_response):
-# The application is suposed to return a png map
-# for the given url:
-#
-# localhost/ms-wsgi?service=WMS&version=1.3.0&request=GetMap
-#
-# For that default request parameters are given.
-#
-# The here used test_mapfile.map is a copy
-# of the example1-1.map mapfile, to the base_path
-# location.
-# 
-# Original location:
-#     ms4w/apps/tutorial/htdocs/example1-1.map
-# 
-# The atribute SHAPEPATH must be change according
-# with the new location:
-# 
-# SHAPEPATH      "ms4w/apps/tutorial/data"
-# 
-# The folowing tags had been added to the
-# MAP tag of the original mapfile:
-# 
-# PROJECTION # Projection definition:
-#    "init=epsg:4326"
-# END # End of the output Projection definition ---
-# 
-# WEB
-#   METADATA
-#     "ows_enable_request" "*"
-#   END
-# END
-
     for key in MAPSERV_ENV:
         if key in env:
             os.environ[key] = env[key]
         else:
             os.unsetenv(key)
 
-    filename = os.path.join( local_settings['base_path'], 'test_mapfile.map')
-
+    # Using a statically assigned mapfile:
+    filename = os.path.join( settings_local['base_path'], 'test_mapfile.map')
     mapfile = mapscript.mapObj(filename)
 
     req = mapscript.OWSRequest()
@@ -96,6 +97,8 @@ def application(env, start_response):
     return [result]
 
 def set_default_parameters(req):
+    # Parameters from url overwrite the default
+    # values.
     default_p = {
         "LAYERS": 'states',
         # The folowing crs (EPSG:4326) require lon/lat bbox
@@ -111,15 +114,9 @@ def set_default_parameters(req):
     }
 
     for p_name, p_value in default_p.iteritems():
-        # Parameters from url overwrite the default
-        # values:
         if req.getValueByName(p_name) is None:
             req.setParameter(p_name, p_value)
             
-    #print("url query: {}".format(env['QUERY_STRING']))
-    #print("request parameters: {}".format(
-    #    [(req.getName(i), req.getValue(i)) for i in range(req.NumParams)]
-    #))
 
 
 if __name__ == '__main__':  # run inline using WSGI reference implementation
